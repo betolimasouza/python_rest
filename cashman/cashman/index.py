06c90f592911f8e76df02791b7517e91efce6eee
Roberto de Lima <robertodelimasouza@gmail.com>
2018-02-19 22:32:14 -0300

    Better endpoints

--- cashman/cashman/index.py
+++ cashman/cashman/index.py
@@ -1,18 +1,53 @@
 from flask import Flask, jsonify, request
 
+from cashman.model.expense import Expense, ExpenseSchema
+from cashman.model.income import Income, IncomeSchema
+from cashman.model.transaction_type import TransactionType
+
 app = Flask(__name__)
 
-incomes = [
-  { 'description': 'salary', 'amount': 5000 }
+
+transactions = [
+  Income('Salary', 5000),
+  Income('Dividends', 200),
+  Expense('pizza', 50),
+  Expense('Rock Concert', 100)
 ]
 
-#GET
-@app.route('/incomes') #registra a view
-def get_incomes():
-  return jsonify(incomes)
 
-#POST
-@app.route('/incomes', methods=['POST'])
+@app.route('/incomes') #GET receitas
+def get_incomes():
+  schema = IncomeSchema(many=True)
+  incomes = schema.dump( 
+    filter(lambda t: t.type == TransactionType.INCOME, transactions)
+  )
+  return jsonify(incomes.data) 
+
+
+@app.route('/incomes', methods=['POST']) #POST receitas
 def add_income():
-  incomes.append(request.get_json())
-  return '', 204
+  income = IncomeSchema().load(request.get_json()) #JSON -> Dados
+  transactions.append(income.data) #Insere dados
+  return "", 204 # Codigo de Status HTTP 204 - The server has successfully fulfilled the request
+
+
+
+@app.route('/expenses')
+def get_expenses():
+  #seleciona todas as transacoes com o tipo INCOME
+  schema = ExpenseSchema(many=True)
+  expenses = schema.dump(
+      filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+  )
+  return jsonify(expenses.data)
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+  expense = ExpenseSchema().load(request.get_json())
+  transactions.append(expense.data)
+  return "", 204
+
+
+if __name__ == "__main__":
+    app.run()
